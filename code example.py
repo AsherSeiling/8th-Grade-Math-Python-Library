da3cf2f2206d3bd3a25f957f0da09148792210df
AsherSeiling <f14asher@gmail.com>
2020-05-25 22:00:59 -0500

    N/A CMD commit test

--- code example.py
+++ code example.py
@@ -1,4 +1,5 @@
 from Eighth_grade_math import eighth_grade_math_solver
 #this is madatory for Slope to standerd and Pythageran theorem
+print()
 import math, time
 eighth_grade_math_solver.volume_of_cubes()
